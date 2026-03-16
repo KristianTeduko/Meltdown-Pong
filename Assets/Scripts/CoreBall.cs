40c6e0de18c60cc72efc866a03689a5e199c8691
KristianTeduko <kristian.turtiainen@student.eduko.fi>
2026-03-16 13:30:31 +0200

    added ball bounce

--- Assets/Scripts/CoreBall.cs
+++ Assets/Scripts/CoreBall.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoreBall : MonoBehaviour
@@ -18,19 +19,76 @@ public class CoreBall : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        StartRandomBallMovement();
+        
+    }
+
+
+    public void StartRandomBallMovement()
+    {
         randomDirectionNumber = Random.Range(0, 4);
         Debug.Log("pablo roation");
         ballRotation = directions[randomDirectionNumber]; // get random direction for the ball
 
         movement = new Vector2(Speed, 0).normalized;
+
         Debug.Log("it moves");
+
         Vector2 direction = Quaternion.Euler(0, 0, ballRotation) * Vector2.right; // add speed to ball
         PlayerRigidbod2D.AddForce(direction, ForceMode2D.Impulse);
+
+
     }
+
+
+    private int lastDirection = -1;
+
+    public void RandomBallMovement()
+    {
+        int newDirection;
+
+        // reroll until direction is different from last
+        do
+        {
+            newDirection = Random.Range(0, directions.Length);
+        }
+        while (newDirection == lastDirection);
+
+        lastDirection = newDirection;
+
+        ballRotation = directions[newDirection];
+
+        Vector2 direction = Quaternion.Euler(0, 0, ballRotation) * Vector2.right;
+
+        // 90° rotation
+        Vector2 perpendicular = new Vector2(-direction.y, -direction.x);
+
+        PlayerRigidbod2D.AddForce(perpendicular * Speed, ForceMode2D.Impulse);
+    }
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void FixedUpdate()
+    {
+        PlayerRigidbod2D.linearVelocity = PlayerRigidbod2D.linearVelocity.normalized * Speed;
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Walls")
+        {
+            Debug.Log("osumaoli hyvä");
+            //RandomBallMovement();
+
+        }
     }
 }
